171d3a144d8cad16ae5a1b18470b6cf7a9485206
Anastasia Perez <Stasiperez85@gmail.com>
2022-02-17 15:06:46 -0700

    add test for employee get(name, id, email, role) and write code to pass test

--- welcome-to-my-team/__test__/Employee.test.js
+++ welcome-to-my-team/__test__/Employee.test.js
@@ -1,10 +1,29 @@
+const { expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
+const employee = new Employee('ellie', '235689', 'ellie@sillymail.com', 'employee');
 
 test('creates the employee information', () => {
-    const employee = new Employee('ellie', '235689', 'ellie@sillymail.com');
 
     expect(employee.name).toBe('ellie');
     expect(employee.id).toBe('235689');
     expect(employee.email).toBe('ellie@sillymail.com');
+    expect(employee.role).toBe('employee')
 });
+
+test('test if it can get name', () => {
+    expect(employee.getName()).toBe('ellie');
+});
+
+test('test if it can get id', () => {
+    expect(employee.getId()).toBe('235689');
+});
+
+test('test if it can get email', () => {
+    expect(employee.getEmail()).toBe('ellie@sillymail.com');
+});
+
+test('test if it can get role', () => {
+    expect(employee.getRole()).toBe('employee');
+});
+
 
