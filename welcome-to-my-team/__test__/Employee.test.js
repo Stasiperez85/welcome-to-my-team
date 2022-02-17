15040113fb33a1b3eea46be1f5c3140d27df6357
Anastasia Perez <Stasiperez85@gmail.com>
2022-02-17 16:09:03 -0700

    add test for magaer to fail, then write code for it to pass

--- welcome-to-my-team/__test__/Employee.test.js
+++ welcome-to-my-team/__test__/Employee.test.js
@@ -1,5 +1,5 @@
-const { expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
+
 const employee = new Employee('ellie', '235689', 'ellie@sillymail.com', 'employee');
 
 test('creates the employee information', () => {
@@ -7,7 +7,7 @@ test('creates the employee information', () => {
     expect(employee.name).toBe('ellie');
     expect(employee.id).toBe('235689');
     expect(employee.email).toBe('ellie@sillymail.com');
-    expect(employee.role).toBe('employee')
+    expect(employee.role).toBe('employee');
 });
 
 test('test if it can get name', () => {
@@ -25,5 +25,6 @@ test('test if it can get email', () => {
 test('test if it can get role', () => {
     expect(employee.getRole()).toBe('employee');
 });
+
 
 
---
+++ welcome-to-my-team/__test__/Manager.test.js
@@ -0,0 +1,33 @@
+const Manager = require('../lib/Manager');
+
+const manager = new Manager('ellie', '235689', 'ellie@sillymail.com', 'Manager', '555-555-5555');
+
+test('creates the employee information', () => {
+
+    expect(manager.name).toBe('ellie');
+    expect(manager.id).toBe('235689');
+    expect(manager.email).toBe('ellie@sillymail.com');
+    expect(manager.role).toBe('Manager');
+    expect(manager.officeNumber).toBe('555-555-5555');
+});
+
+test('test if it can get name', () => {
+    expect(manager.getName()).toBe('ellie');
+});
+
+test('test if it can get id', () => {
+    expect(manager.getId()).toBe('235689');
+});
+
+test('test if it can get email', () => {
+    expect(manager.getEmail()).toBe('ellie@sillymail.com');
+});
+
+test('test if it can get role', () => {
+    expect(manager.getRole()).toBe('Manager');
+});
+
+test('test if it can get office number', () => {
+    expect(manager.getOfficeNumber()).toBe('555-555-5555');
+});
+
