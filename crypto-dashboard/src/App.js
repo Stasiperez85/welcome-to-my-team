264e47ba6055ab00b4d5db00041194e1cb21d098
Stasiperez85 <Stasiperez85@gmail.com>
2022-09-07 22:00:58 -0600

    updated start files

---
+++ crypto-dashboard/src/App.js
@@ -0,0 +1,14 @@
+
+import NewsFeed from "./components/NewsFeed";
+import CurrencyConverter from "./components/CurrencyConverter"
+
+const App = () => {
+  return (
+    <div>
+      <NewsFeed />
+      <CurrencyConverter />
+    </div>
+  );
+}
+
+export default App;
---
+++ crypto-dashboard/src/components/ExchangeRate.js
@@ -0,0 +1,9 @@
+const ExchangeRate = () => {
+    return (
+        <div className="exchange-rate">
+
+        </div>
+    );
+}
+
+export default ExchangeRate;
