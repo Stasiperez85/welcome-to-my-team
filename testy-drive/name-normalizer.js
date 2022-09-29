38075e7dcf7a71a412e7bfef83fdfc8486d59638
Stasiperez85 <Stasiperez85@gmail.com>
2022-09-28 22:08:15 -0600

    run more test

--- testy-drive/name-normalizer.js
+++ testy-drive/name-normalizer.js
@@ -4,11 +4,31 @@ const normalize = (name) => {
         return name;
     }
 
-    const lastName = names[names.length - 1];
-    const namesOtherThanLast = names.slice(0, -1)
+    const firstName = first(names);
+    let middleName;
+    let middleInitial;
+    let lastName;
 
-    return `${lastName}, ${namesOtherThanLast.join(', ')}`;
-}
+    if (names.length === 3) {
+        middleName = names[1];
+        middleInitial = initialize(middleName);
+        lastName = names[2];
+    } else {
+        lastName = names[1];
+    }
 
+    return `${lastName}, ${firstName}${middleInitial ? `${middleInitial}` : ''}`;
+};
+
+const first = array => array[0];
+const last = array => array[array.length - 1];
+
+const initialize = name => {
+    if (name.length > 1) {
+        return `${name.slice(0, 1)}.`;
+    } else {
+        return name;
+    }
+};
 
 module.exports = normalize;
